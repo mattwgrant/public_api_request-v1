c411894b9c684f7145d637169d7f5f4a1b39e869
Matthew Grant <amattwgrant@gmail.com>
2020-09-11 15:57:27 -0400

    building cards, data pulling but not displaying

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,20 @@
-const button = document.querySelector('h1');
-const body = document.querySelector('body');
+const gallery = document.getElementById('gallery');
 
-button.addEventListener('click', () => {
-	if ( body.style.backgroundColor !== 'red' ) {
-		body.style.backgroundColor = 'red';
-	} else {
-		body.style.backgroundColor = 'rgba(235, 235, 235, 0.9)';
-	}
-})
+fetch('https://randomuser.me/api/?results=12')
+	.then(res => res.json())
+	.then(data => console.log(data))
+
+
+function generateCards(data) {
+	const employees = data.map(employee => `
+		<div class="card-img-container">
+			<img class="card-img" src="${employee.result.picture.thumbnail}" alt="profile picture">
+		</div>
+		<div class=card-info-container>
+			<h3 id="name" class="card-name cap">${employee.results.name.first} ${employee.results.name.last}</h3>
+			<p class="card-text">${employee.results.email}</p>
+			<p class="card-text cap">${employee.results.location.city}, ${employee.results.location.state}</p>
+		</div>
+		`)
+	gallery.appendChild(employee);
+}
