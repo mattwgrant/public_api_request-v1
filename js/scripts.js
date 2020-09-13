423b27deb3d75c96e94df1fb9f278f65de156ea7
Matthew Grant <amattwgrant@gmail.com>
2020-09-13 15:19:28 -0400

    modal info displaying in console on click but for all users being displayed not just the target

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,17 @@
 const gallery = document.getElementById('gallery');
 const cards = document.querySelectorAll('.card');
-// const cardsArr = Array.from(cards);
-// const modal = document.querySelectorAll('.modal');
+const userData = [];
 const url = 'https://randomuser.me/api/?nat=us&results=12';
 /*
  * Calls API to pull data from 12 random users
  */
-fetch(url)
+fetch(url, {mode: 'cors'})
 	.then(res => res.json())
 	.then(data => {
 		const users = data;
+		userData.push(users);
 		generateCards(users);
-		generateModals(users);
+		// generateModals(users);
 		// console.log(users);
 		})
 	.catch(error => console.log('There was an error with your request:', error))
@@ -42,24 +42,28 @@ function generateCards(data) {
  */
 function generateModals(data) {
 
-	const users = data.results.map(user => `
-		<div class="modal-container">
+	// const users = data.map(user => 
+	const userInfo = userData[0].results;
+ 	for ( let i = 0; i < userInfo.length; i++ ) {	
+		
+		const modal = `<div class="modal-container">
 	        <div class="modal">
 	            <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
 	            <div class="modal-info-container">
-	                <img class="modal-img" src="${user.picture.large}" alt="profile picture">
-	                <h3 id="name" class="modal-name cap">${user.name.first} ${user.name.last}</h3>
-	                <p class="modal-text">${user.email}</p>
-	                <p class="modal-text cap">${user.location.city}</p>
+	                <img class="modal-img" src="${userInfo[i].picture.large}" alt="profile picture">
+	                <h3 id="name" class="modal-name cap">${userInfo[i].name.first} ${userInfo[i].name.last}</h3>
+	                <p class="modal-text">${userInfo[i].email}</p>
+	                <p class="modal-text cap">${userInfo[i].location.city}</p>
 	                <hr>
-	                <p class="modal-text">${user.cell}</p>
-	                <p class="modal-text">${user.location.street.number} ${user.location.street.name}, ${user.location.city}, ${user.location.state} ${user.location.postcode}</p>
-	                <p class="modal-text">Birthday: ${user.dob.date.substring(0,10)}</p>
+	                <p class="modal-text">${userInfo[i].cell}</p>
+	                <p class="modal-text">${userInfo[i].location.street.number} ${userInfo[i].location.street.name}, ${userInfo[i].location.city}, ${userInfo[i].location.state} ${userInfo[i].location.postcode}</p>
+	                <p class="modal-text">Birthday: ${userInfo[i].dob.date.substring(0,10)}</p>
 	            </div>
 	        </div>
-	    </div>
-	`).join('')
-	gallery.insertAdjacentHTML('beforeend', users)
+	    </div>`
+	
+		console.log(modal)
+	}
 }
 
 
@@ -68,19 +72,20 @@ function generateModals(data) {
  */
 gallery.addEventListener('click', (e) => {
  	
- 	// for ( let i = 0; i < cardsArr.length; i++ ) {
  		if ( e.target && e.target.className !== 'gallery') {
-			// generateModals(e.target);
-			console.log('hi');
+			// generateModals(userData);
+			// console.log('hi');
 			// using console.log to just test that the click was working
+			// console.log(userData[0].results[i]);
+			generateModals(userData)
  		} 
- 	// }	
+ 		
  });
 
-document.querySelectorAll('.modal-container').addEventListener('click', (e) => {
- 	// const modal = document.querySelectorAll('.modal-container');
- 	if ( e.target.nodeName = 'BUTTON' ) {
- 		modal.style.display = 'none';
- 	}
- });
+// document.querySelectorAll('.modal-container').addEventListener('click', (e) => {
+//  	// const modal = document.querySelectorAll('.modal-container');
+//  	if ( e.target.nodeName = 'BUTTON' ) {
+//  		modal.style.display = 'none';
+//  	}
+//  });
 
