3efd22d40a646c520c0ef2ad507f8f1e93b2c8f7
Matthew Grant <amattwgrant@gmail.com>
2020-09-15 13:36:02 -0400

    still working on modal display

---
+++ js/app.js
@@ -0,0 +1,102 @@
+const gallery = document.getElementById('gallery');
+let cardsList = [];
+const url = 'https://randomuser.me/api/?nat=us&results=12';
+// let userData = [];
+let name = document.querySelector('h3.value');
+
+/*
+ * Calls API to pull data from 12 random users
+ */
+ // , {mode: 'cors'}
+fetch(url)
+	.then(status)
+	.then(res => res.json())
+	.then(data => {
+		const users = data;
+		// userData.push(users);
+		generateCards(users);
+		generateModals(users);
+		console.log(users);
+		})
+	.catch(error => console.log('There was an error with your request:', error))
+
+
+/*********************
+	HELPERS
+*********************/
+
+/*
+ * Checks the status of the promise
+ */
+function status(response) {
+	if (response.ok) {
+		return Promise.resolve(response); 
+	} else {
+		return Promise.reject(new Error(response.statusText));
+	}
+}
+
+/*
+ * Creates HTML to display as card for each user
+ */
+function generateCards(data) {
+	const cards = data.results.map(user => `
+		<div class="card">
+			<div class="card-img-container">
+				<img class="card-img" src="${user.picture.thumbnail}" alt="profile picture">
+			</div>
+			<div class=card-info-container>
+				<h3 id="name" class="card-name cap">${user.name.first} ${user.name.last}</h3>
+				<p class="card-text">${user.email}</p>
+				<p class="card-text cap">${user.location.city}, ${user.location.state}</p>
+			</div>
+		</div>
+		`).join('')
+		cardsList.push(cards);
+	gallery.insertAdjacentHTML('beforeend', cards);
+
+
+}
+
+
+/* 
+ * Generates HTML and content for the modals
+ */
+function generateModals(data) {
+	
+
+	const modals = data.results.map(user => 
+		// const birthday = user.results.dob.date;
+		// const day = birthday.slice(8, 10);
+		// const month = birthday.slice(5, 7);
+		// const year = birthday.slice(0, 4);
+	
+		`<div class="modal-container">
+	        <div class="modal">
+	            <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+	            <div class="modal-info-container">
+	                <img class="modal-img" src="${user.picture.large}" alt="profile picture">
+	                <h3 id="name" class="modal-name cap">${user.name.first} ${user.name.last}</h3>
+	                <p class="modal-text">${user.email}</p>
+	                <p class="modal-text cap">${user.location.city}</p>
+	                <hr>
+	                <p class="modal-text">${user.cell.replace('-', ' ')}</p>
+	                <p class="modal-text">${user.location.street.number} ${user.location.street.name}, ${user.location.city}, ${user.location.state} ${user.location.postcode}</p>
+	                <p class="modal-text">Birthday: ${user.dob.date.substring(0,10).replace(/'-'/g, '/')}</p>
+	            </div>
+	        </div>
+	    </div>`
+	).join('')
+	
+	console.log(modals);
+}
+
+/*********************
+	EVENT LISTENERS
+*********************/
+
+gallery.addEventListener('click', (e) => {
+	if ( e.target.className !== 'gallery' ) {
+		console.log('hi');
+	}
+});
--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,10 @@
 const gallery = document.getElementById('gallery');
-const cards = document.querySelectorAll('.card');
+// const cardDiv = document.querySelectorAll('.card');
 let userData = [];
 const url = 'https://randomuser.me/api/?nat=us&results=12';
 let modals = [];
+let cards = [];
+
 /*
  * Calls API to pull data from 12 random users
  */
@@ -12,7 +14,8 @@ fetch(url, {mode: 'cors'})
 		const users = data;
 		userData.push(users);
 		generateCards(users);
-		console.log(users);
+		// generateModals(users);
+		// console.log(users);
 		})
 	.catch(error => console.log('There was an error with your request:', error))
 
@@ -22,19 +25,45 @@ fetch(url, {mode: 'cors'})
  * Creates HTML to display as card for each user
  */
 function generateCards(data) {
-	const employees = data.results.map(employee => `
+	const userInfo = userData[0].results;
+	for ( let i = 0; i < userInfo.length; i++ ) {	
+		let card = `
 		<div class="card">
 			<div class="card-img-container">
-				<img class="card-img" src="${employee.picture.thumbnail}" alt="profile picture">
+				<img class="card-img" src="${userInfo[i].picture.thumbnail}" alt="profile picture">
 			</div>
 			<div class=card-info-container>
-				<h3 id="name" class="card-name cap">${employee.name.first} ${employee.name.last}</h3>
-				<p class="card-text">${employee.email}</p>
-				<p class="card-text cap">${employee.location.city}, ${employee.location.state}</p>
+				<h3 id="name" class="card-name cap">${userInfo[i].name.first} ${userInfo[i].name.last}</h3>
+				<p class="card-text">${userInfo[i].email}</p>
+				<p class="card-text cap">${userInfo[i].location.city}, ${userInfo[i].location.state}</p>
 			</div>
 		</div>
-		`).join('')
-	gallery.insertAdjacentHTML('beforeend', employees);
+		`
+		cards.push(card);
+		gallery.insertAdjacentHTML('beforeend', card);
+
+		const birthday = userInfo[i].dob.date;
+		const day = birthday.slice(8,10);
+		const month = birthday.slice(5, 7);
+		const year = birthday.slice(0,4);
+
+		let modal = `<div class="modal-container">
+	        <div class="modal">
+	            <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+	            <div class="modal-info-container">
+	                <img class="modal-img" src="${userInfo[i].picture.large}" alt="profile picture">
+	                <h3 id="name" class="modal-name cap">${userInfo[i].name.first} ${userInfo[i].name.last}</h3>
+	                <p class="modal-text">${userInfo[i].email}</p>
+	                <p class="modal-text cap">${userInfo[i].location.city}</p>
+	                <hr>
+	                <p class="modal-text">${userInfo[i].cell.replace('-', ' ')}</p>
+	                <p class="modal-text">${userInfo[i].location.street.number} ${userInfo[i].location.street.name}, ${userInfo[i].location.city}, ${userInfo[i].location.state} ${userInfo[i].location.postcode}</p>
+	                <p class="modal-text">Birthday: ${month}/${day}/${year}</p>
+	            </div>
+	        </div>
+	    </div>`
+	    modals.push(modal);
+	}
 }
 
 
@@ -44,48 +73,20 @@ function generateCards(data) {
 /*
  * Click events to create and openthe modals
  */
+
 gallery.addEventListener('click', (e) => {
  	
- 	const userInfo = userData[0].results;
- 		if ( e.target && e.target.className !== 'gallery') {
-
-			for ( let i = 0; i < userInfo.length; i++ ) {	
-				const birthday = userInfo[i].dob.date;
-				const day = birthday.slice(8,10);
-				const month = birthday.slice(5, 7);
-				const year = birthday.slice(0,4);
-
-				let modal = `<div class="modal-container">
-			        <div class="modal">
-			            <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-			            <div class="modal-info-container">
-			                <img class="modal-img" src="${userInfo[i].picture.large}" alt="profile picture">
-			                <h3 id="name" class="modal-name cap">${userInfo[i].name.first} ${userInfo[i].name.last}</h3>
-			                <p class="modal-text">${userInfo[i].email}</p>
-			                <p class="modal-text cap">${userInfo[i].location.city}</p>
-			                <hr>
-			                <p class="modal-text">${userInfo[i].cell.replace('-', ' ')}</p>
-			                <p class="modal-text">${userInfo[i].location.street.number} ${userInfo[i].location.street.name}, ${userInfo[i].location.city}, ${userInfo[i].location.state} ${userInfo[i].location.postcode}</p>
-			                <p class="modal-text">Birthday: ${month}/${day}/${year}</p>
-			            </div>
-			        </div>
-			    </div>`
-		
-			modals.push(modal);	
-				
-		 	} 
-		 	for ( let j = 0; j < modals.length; j++ ) {
-		 		if (modals.index === userInfo.index ) {
-		 			gallery.innerHTML = modals[j];
-		 		}
-		 	}
- 	}	
+	if ( e.target && e.target.className !== 'gallery') {
+		gallery.insertAdjacentHTML('beforeend', modals);
+	} else {
+		console.log('Not right!');
+	}
  });
 
-// document.querySelectorAll('.modal-container').addEventListener('click', () => {
-//  	// const modal = document.querySelectorAll('.modal-container');
-//  	// if ( e.target.nodeName = 'BUTTON' ) {
+// document.querySelectorAll(modals).addEventListener('click', (e) => {
+//  	const modal = document.querySelectorAll('.modal-container');
+//  	if ( e.target.nodeName == 'BUTTON' ) {
 //  		modal.style.display = 'none';
-//  	// }
+//  	}
 //  });
 
